ecd5f4034312471d84812753db57cbd5eedf561b
Joseph Pagan <joe@Josephs-MacBook-Pro.local>
2022-02-04 11:45:27 +0000

    .

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import InstantSearch from 'vue-instantsearch/vue3/es';
+import algoliasearch from 'algoliasearch/lite';
 
 createApp(App).mount('#app')
