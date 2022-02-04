8afe630489c7a8ff76818952fcb5c2793c5f7748
Joseph Pagan <joe@Josephs-MacBook-Pro.local>
2022-02-04 13:08:43 +0000

    .

--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,7 @@ export default ({ command }) => ({
       // ],
       // external: ['crypto', 'http', 'https', 'url'],
       input: {
-        main: 'src/ts/main.js',
+        main: '/src/ts/main.js',
       },
       output: {
         sourcemap: true,
