88c5731948e27e63fe0bf26d596caf7856811d25
Joseph Pagan <joe@Josephs-MacBook-Pro.local>
2022-02-04 11:55:40 +0000

    .

---
+++ .eslintrc.js
@@ -0,0 +1,94 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:vue/essential',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: "@typescript-eslint/parser",
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+    'import',
+  ],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        // ts: 'never',
+        // tsx: 'never',
+      },
+    ],
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: [
+          'state',
+        ],
+      },
+    ],
+    "max-len": ["warn"],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn"]
+  },
+  settings: {
+    'import/extensions': [
+      '.js',
+      '.jsx',
+      '.ts',
+      '.tsx',
+    ],
+    // 'import/parsers': {
+    //   '@typescript-eslint/parser': ['.ts', '.tsx'],
+    // },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+      // node: {
+      //   extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      //   moduleDirectory: [
+      //     './node_modules',
+      //     './src/',
+      //   ],
+      // },
+      // alias: {
+      //   map: [
+      //     ['@', './src'],
+      //   ],
+      //   extensions: ['.ts', '.js', '.jsx', '.json'],
+      // },
+    },
+  },
+  // overrides: [
+  //   {
+  //     files: ["*.ts", "*.tsx"],
+  //     extends: [
+  //       'airbnb-base',
+  //       'airbnb-typescript/base',
+  //       'plugin:vue/essential',
+  //       'plugin:vue/vue3-recommended',
+  //       // 'plugin:@typescript-eslint/eslint-recommended',
+  //       // "plugin:@typescript-eslint/recommended",
+  //     ],
+  //     parser: "@typescript-eslint/parser",
+  //     plugins: ["@typescript-eslint"],
+  //   },
+  // ],
+};
